05031a0daec933224d386d1bbb4b6662e250c515
Eugene MInaev <administrator@mbp-eugene.home>
2024-07-06 18:52:48 +0200

    code refactoring, some adjustments added

--- conftest.py
+++ conftest.py
@@ -1,45 +1,29 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='en', help="Choose language: en, es, fr, etc.")
 
 @pytest.fixture(scope="function")
-def browser(request):
+def user_language(request):
+    return request.config.getoption("language")
+
+@pytest.fixture(scope="function")
+def browser(request, user_language):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
 
     yield browser
     browser.quit()
-
-
-    # browser = None
-    # if browser_name == "chrome":
-    #     print("\nstart chrome browser for test..")
-    #     browser = webdriver.Chrome()
-    # elif browser_name == "firefox":
-    #     print("\nstart firefox browser for test..")
-    #     browser = webdriver.Firefox()
-    # else:
-    #     raise pytest.UsageError("--browser_name should be chrome or firefox")
-    # yield browser
-    # print("\nquit browser..")
-    # browser.quit()
-
-# options = Options()
-# language = browser.find_element(By.TAG_NAME, 'language')
-# options.add_experimental_option('prefs', {'intl.accept_languages': language})
-# browser = webdriver.Chrome(options=options)
--- test_items.py
+++ test_items.py
@@ -1,34 +1,11 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_add_item_to_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
-
-# def test_guest_should_see_login_link_fail(browser):
-#     browser.get(link)
-#     browser.find_element(By.CSS_SELECTOR, "#magic_link")
-# def test_task_locales(browser):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     browser.get(link)
-
-    button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert "Ajouter au panier" in button.text
-    # button.click()
-
-
-
+    # Визуально проверяем, что язык страницы соответствует заданному параметру запуска теста
+    time.sleep(5)
+    # Проверяем наличие кнопки добавления в корзину
+    assert browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
