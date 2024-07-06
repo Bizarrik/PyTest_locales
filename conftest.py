b0ba0de881bf544c05b17a712e8259d3d8b9f9cf
Eugene MInaev <administrator@mbp-eugene.home>
2024-07-06 18:18:57 +0200

    readme file edited

--- conftest.py
+++ conftest.py
@@ -1,40 +1,45 @@
-# Создайте GitHub-репозиторий, в котором будут лежать файлы conftest.py и test_items.py.
-# Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
-# Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
-# В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину. Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
-# Тест должен запускаться с параметром language следующей командой:
-# pytest --language=es test_items.py
-# и проходить успешно. Достаточно, чтобы код работал только для браузера Сhrome.
-# Отправить ссылку на данный репозиторий в качестве ответа на данное задание.
-# Отправить решение на рецензирование другим учащимся. Не забудьте, что решение на рецензирование можно отправить только один раз.
-# Проверьте решения минимум трех других учащихся, чтобы получить баллы за задание.
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en', help="Choose language: en, es, fr, etc.")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
-    print("\nquit browser..")
     browser.quit()
 
-options = Options()
-language = browser.find_element(By.TAG_NAME, 'language')
-options.add_experimental_option('prefs', {'intl.accept_languages': language})
-browser = webdriver.Chrome(options=options)
+
+    # browser = None
+    # if browser_name == "chrome":
+    #     print("\nstart chrome browser for test..")
+    #     browser = webdriver.Chrome()
+    # elif browser_name == "firefox":
+    #     print("\nstart firefox browser for test..")
+    #     browser = webdriver.Firefox()
+    # else:
+    #     raise pytest.UsageError("--browser_name should be chrome or firefox")
+    # yield browser
+    # print("\nquit browser..")
+    # browser.quit()
+
+# options = Options()
+# language = browser.find_element(By.TAG_NAME, 'language')
+# options.add_experimental_option('prefs', {'intl.accept_languages': language})
+# browser = webdriver.Chrome(options=options)
--- test_items.py
+++ test_items.py
@@ -13,6 +13,22 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-def test_task_stepik(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
+    browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+
+# def test_guest_should_see_login_link_fail(browser):
+#     browser.get(link)
+#     browser.find_element(By.CSS_SELECTOR, "#magic_link")
+# def test_task_locales(browser):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     browser.get(link)
+
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert "Ajouter au panier" in button.text
+    # button.click()
+
+
+
